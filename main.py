f70feeeb09986095d796cf862dad3dee6d55adc5
suttpow <suttipow@gmail.com>
2024-04-12 13:10:31 +0700

    add

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import streamlit as st
+from flask import Flask, render_template, request
+import pickle
+import pandas as pd
+import sklearn
 
 st.title("Getting started streamlit")
 st.write("test")
