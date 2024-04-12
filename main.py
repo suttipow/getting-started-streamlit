07d08b3e60a66faaa4fd5049c5124fc6b2462821
suttpow <suttipow@gmail.com>
2024-04-12 14:28:11 +0700

    update

--- main.py
+++ main.py
@@ -4,12 +4,53 @@ import pickle
 import pandas as pd
 import sklearn
 
-st.title("Getting started streamlit")
-st.write("test main app2")
+def transform_age(ages):
+    new_data = []
+    for i in ages:
+        if i >= 46 :
+            new_data.append(3)
+        elif i >= 26:
+            new_data.append(2)
+        else:
+            new_data.append(1)
+    return new_data
 
-app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return("Hello world")
+def transform_hangout(htimes):
+    new_data = []
+    for i in htimes:
+        if i > 2 :
+            new_data.append(1)
+        else :
+            new_data.append(0)
+    return new_data
 
+def transform_data(data):
+    data['age'] = transform_age(data['age'])
+    data['hang_out'] = transform_hangout(data['hang_out'])
+    data['gender'] = [ 0 if i == 'Female'   else 1  for i in data['gender']  ]
+    return data
+
+def predict(new_data):
+    result =''
+    filename = 'svm_model.clf'
+    clf = pickle.load(open(filename, 'rb'))
+    new_df = pd.DataFrame(new_data)
+    result = clf.predict(new_df)
+    #print(result)
+    return 'Not apply for membership' if result[0] == 0 else 'Apply to membership'
+
+st.title("Membership Prediction App")
+
+age = st.number_input("Enter your age:", min_value=0)
+gender = st.selectbox("Select your gender", ["Male", "Female"])
+htime = st.number_input("Enter your average weekly hangout time (hours):", min_value=0)
+
+if st.button("Predict Membership Eligibility"):
+  try:
+    new_data = {'age': [age], 'gender': [gender], 'hang_out': [htime]}
+    new_data = transform_data(new_data)
+    result = predict(new_data)
+    st.write(result)
+  except ValueError:
+    st.error("Please enter valid numbers for age and hangout time.")
