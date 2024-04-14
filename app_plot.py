365eebdcd7f918035b6817fe69228834e1e67b82
suttpow <suttipow@gmail.com>
2024-04-14 11:38:18 +0700

    update

---
+++ app_plot.py
@@ -0,0 +1,21 @@
+import pandas as pd  # pip install pandas openpyxl
+import plotly.express as px  # pip install plotly-express
+import streamlit as st  # pip install streamlit
+
+# emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
+st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout="wide")
+
+# ---- READ EXCEL ----
+
+df = pd.read_excel(
+    io="supermarkt_sales.xlsx",
+    engine="openpyxl",
+    sheet_name="Sales",
+    skiprows=3,
+    usecols="B:R",
+    nrows=1000,
+)
+
+
+# ---- MAINPAGE ----
+print(df)
