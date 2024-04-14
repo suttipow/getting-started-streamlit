2ed9202f3cff19500ec18eade7195abe988c67af
suttpow <suttipow@gmail.com>
2024-04-14 21:43:33 +0700

    update

--- app_plot.py
+++ app_plot.py
@@ -62,7 +62,7 @@ st.markdown("##")
 # TOP KPI's
 total_sales = int(df_selection["Total"].sum())
 average_rating = round(df_selection["Rating"].mean(), 1)
-star_rating = ":red_triangle_pointed_down:" * int(round(average_rating, 0))
+star_rating = ":star:" * int(round(average_rating, 0))
 average_sale_by_transaction = round(df_selection["Total"].mean(), 2)
 
 left_column, middle_column, right_column = st.columns(3)
